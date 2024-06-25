6e1c6c61a6e2f9e6a335db48c46365d413947eae
alireza <arkh919@gmail.com>
2024-06-25 13:46:40 -0400

    code challange 1 done

--- script.js
+++ script.js
@@ -122,20 +122,62 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //   } else {
 //     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(el)}`);
 //   }
-// }) 
+// })
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // currencies.forEach(function (value, key, map) {
 //   console.log(`The currency ${key} stands for ${value}`);
 // })
 
-const currenciesun = new Set(["USD", "USD", "EUR", "GBP", "EUR", "EUR", "GBP"]);
+// const currenciesun = new Set(["USD", "USD", "EUR", "GBP", "EUR", "EUR", "GBP"]);
 
-currenciesun.forEach(function (value, _, set) {
-  console.log(`${value}: ${value}`);
-});
+// currenciesun.forEach(function (value, _, set) {
+//   console.log(`${value}: ${value}`);
+// });
+
+
+
+/*
+Coding Challenge #1
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners
+about their dog's age, and stored the data into an array (one array for each). For
+now, they are just interested in knowing whether a dog is an adult or a puppy.
+A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years
+old.
+Your tasks:
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages
+('dogsJulia' and 'dogsKate'), and does the following things:
+1. Julia found out that the owners of the first and the last two dogs actually have
+cats, not dogs! So create a shallow copy of Julia's array, and remove the cat
+ages from that copied array (because it's a bad practice to mutate function
+parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1
+is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy
+🐶
+")
+4. Run the function for both test datasets
+Test data:
+§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+Hints: Use tools from all lectures in this section so far 😉
+GOOD LUCK 😀*/
+
+
+const checkDogs = function(dogjulia, dogskate) {
+  let juliacopy = [...dogjulia];
+  juliacopy = juliacopy.splice(1, 2);
+  const goodarr = [...juliacopy, ...dogskate];
+  goodarr.forEach(function (age, index) {
+    const type = age >= 3 ? "adult" : "puppy"
+    console.log(`The Dog #${index + 1} is ${type} base on its age: ${age}`);
+  })
+}
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
