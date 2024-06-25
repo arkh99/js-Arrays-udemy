83ca31cff3ccae8ee7ba7084c62511edbd52365a
alireza <arkh919@gmail.com>
2024-06-25 12:06:01 -0400

    before bankist

--- script.js
+++ script.js
@@ -65,11 +65,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -120,10 +116,26 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //   }
 // })
 
-movements.forEach(function (el, i) {
-  if (el > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${el}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(el)}`);
-  }
-})
+// movements.forEach(function (el, i) {
+//   if (el > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${el}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(el)}`);
+//   }
+// }) 
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`The currency ${key} stands for ${value}`);
+// })
+
+const currenciesun = new Set(["USD", "USD", "EUR", "GBP", "EUR", "EUR", "GBP"]);
+
+currenciesun.forEach(function (value, _, set) {
+  console.log(`${value}: ${value}`);
+});
